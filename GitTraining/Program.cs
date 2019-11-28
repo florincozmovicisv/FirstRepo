288be8260cf62cc815825a27841464eed02e6e29
Florin Cozmovici <florin.cozmovici@blackline.com>
2019-11-28 14:53:57 +0200

    add UpperCase

--- GitTraining/Program.cs
+++ GitTraining/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GitTraining
 {
@@ -7,6 +8,14 @@ namespace GitTraining
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //prima problema UpperCase
+            UpperCase upperCase = new UpperCase();
+            List<string> namesList = new List<string>() { "anA", "elena", "MARIA", "cRisTiAn" };
+            namesList = upperCase.CapitalizingNames(namesList);
+            foreach (var name in namesList)
+                Console.WriteLine(name);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ GitTraining/UpperCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitTraining
+{
+    public class UpperCase
+    {
+        public List<string> CapitalizingNames(List<string> listOfNames)
+        {
+            for (int i = 0; i < listOfNames.Count(); i++)
+            {
+                char[] nameArray = listOfNames[i].ToArray();
+                int j = 0;
+                while (j < nameArray.Length)
+                {
+                    if (j == 0) nameArray[j] = char.ToUpper(nameArray[j]);
+                    else nameArray[j] = char.ToLower(nameArray[j]);
+                    j++;
+                }
+                listOfNames[i] = new string(nameArray);
+            }
+            return listOfNames;
+        }
+    }
+}
